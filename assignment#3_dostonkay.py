5cbf022b4e318f92675775e7fc7c9342f66e16af
DostonKayimov <82517850+DostonKayimov@users.noreply.github.com>
2021-04-24 17:42:35 +0500

    Add files via upload

---
+++ assignment#3_dostonkay.py
@@ -0,0 +1,28 @@
+#Yearly expenses($) to study in dream University
+
+total_fees = 28000
+my_yearly_income = 2000
+if_I_rob_bank = 25000
+
+if total_fees > my_yearly_income:
+    print("Not that bad! Keep working and you will be applying with your children")
+
+if total_fees < my_yearly_income:
+    print("Congrats! pack your stuff right away")
+
+if total_fees > if_I_rob_bank:
+    print("You almost had ti! Welcome to prison")
+
+if total_fees < if_I_rob_bank:
+    print("Ok, you have 24 hours to leave the country")
+
+if_I_rob_bank += 5000
+
+if total_fees >= if_I_rob_bank:
+    print("You almost had ti! Welcome to prison")
+
+if total_fees <= if_I_rob_bank:
+    print("Ok, you have 24 hours to leave the country")
+
+if total_fees == if_I_rob_bank:
+    print("Be quick, you dont have much time to leave")
